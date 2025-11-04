6862c9950b04dc18d9f579579977ee5f3da0cbc1
Felipe Correia Costa <felipe.correia@gkt.grupokabum.com.br>
2025-11-04 13:29:12 -0300

    Resolver Bug 500 internal error na parte dos IFs entrega tipo_frete

--- app.py
+++ app.py
@@ -4,53 +4,47 @@ app = Flask(__name__)
 
 @app.route("/calcular-frete", methods=['POST'])
 def calcular_frete():
-    print("passo1")
+    
     data = request.get_json()
-    print("passo2")
+    
     if not data or not "dimensao" in data or not "peso" in data or not "tipo_frete" in data:
-        print("passo3")
+        
         return jsonify({"error": "campos não completos ou erro na sintaxe"}), 400
     
     try:
-        print("passo4")
         tipo_frete = data['tipo_frete']
         altura = data['dimensao']['altura']
         largura = data['dimensao']['largura']
         peso = data['peso']
         
-        print(valor_frete)
-        if peso <= 0 :
-            return jsonify({[]})
         
-        #ENTREGA NINJA
-        if (altura < 10 or altura > 200) or (largura < 6 or largura > 140):
-            print("passo5")
-            return jsonify({"erro": "largura ou altura não corresponde ao tamanho permitido - largura = 200, largura = 140"})
-        else:
-            print("passoEXTRA")
-            return jsonify({
-                                                    
-                                "nome":"Entrega Ninja",
-                                "valor_frete": f"{valor_frete:.2f}",
-                                "prazo_dias": 6            
-                            })
+        if tipo_frete == "ninja":
+            if (altura >= 5 or altura <=140) or (largura >= 13 or largura <= 125) or peso >= 0:
+                return jsonify({                       
+                                    "nome":"Entrega Ninja",
+                                    "valor_frete": f"{valor_frete:.2f}",
+                                    "prazo_dias": 6            
+                                }),
             
+            else:
+                return jsonify({"erro": "largura ou altura não corresponde ao tamanho permitido - altura máxima = 140, largura máxima = 125"}),400
+                
+                
+        elif tipo_frete == "kabum":   
+            if (altura >= 10 or altura <=200) or (largura >= 6 or largura <= 140) or peso >= 0:
+            
+                return jsonify({"erro": "largura ou altura não corresponde ao tamanho permitido - altura máxima = 200, largura máxima = 140"}),400
+            else:
+                valor_frete = (peso * 0.2) /10
+                return jsonify({
+                                                        
+                                    "nome":"Entrega Ninja",
+                                    "valor_frete": f"{valor_frete:.2f}",
+                                    "prazo_dias": 4            
+                                }),
     except Exception as e:
         print(f"{e}")
 
-
-    if tipo_frete == "Ninja":   
-        if (altura < 10 or altura > 200) or (largura < 6 or largura > 140):
-        
-            return jsonify({"erro": "largura ou altura não corresponde ao tamanho permitido - largura = 200, largura = 140"})
-        else:
-            valor_frete = (peso * 0.3) /10
-            return jsonify({
-                                                    
-                                "nome":"Entrega Ninja",
-                                "valor_frete": f"{valor_frete:.2f}",
-                                "prazo_dias": 6            
-                            })
             
             
 if __name__ == '__main__':
