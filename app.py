23bb21d5d93245626515941d03c54ce3cebd44e9
Felipe Correia Costa <felipe.correia@gkt.grupokabum.com.br>
2025-11-06 10:21:27 -0300

    Remoção do tipo_frete e melhoria na logica do IF

--- app.py
+++ app.py
@@ -7,38 +7,31 @@ def calcular_frete():
     
     data = request.get_json()
     
-    if not data or not "dimensao" in data or not "peso" in data or not "tipo_frete" in data:
+    if not data or not "dimensao" in data or not "peso" in data:
         return jsonify({"error": "campos não completos ou erro na sintaxe"}), 400
     
     try:
-        tipo_frete = data['tipo_frete']
         altura = data['dimensao']['altura']
         largura = data['dimensao']['largura']
         peso = data['peso']
-        
-        if tipo_frete == "ninja":
-            if (altura >= 10 and altura <=200) and (largura >= 6 and largura <= 140) and peso > 0:
-                
-                valor_frete = (peso * 0.3) /10
-                return jsonify({                       
-                                "nome":"Entrega Ninja",
-                                "valor_frete": f"{valor_frete:.2f}",
-                                "prazo_dias": 6            
-                                })
+        retorno = []
+        if (altura >= 10 and altura <=200) and (largura >= 6 and largura <= 140) and peso > 0:
             
-            else:
-                return jsonify([]),400
-                
-        elif tipo_frete == "kabum":   
-            if (altura >= 5 and altura <=140) and (largura >= 13 and largura <= 125) and peso > 0:
-                valor_frete = (peso * 0.2) /10
-                return jsonify({                        
-                                "nome":"Entrega Kabum",
-                                "valor_frete": f"{valor_frete:.2f}",
-                                "prazo_dias": 4            
-                                })
-            else:
-                return jsonify([]),400
+            valor_frete = (peso * 0.3) /10
+            retorno.append({                       
+                            "nome":"Entrega Ninja",
+                            "valor_frete": f"{valor_frete:.2f}",
+                            "prazo_dias": 6            
+                            })
+                      
+        if (altura >= 10 and altura <=140) and (largura >= 6 and largura <= 140) and peso > 0:
+            valor_frete = (peso * 0.2) /10
+            retorno.append({                        
+                            "nome":"Entrega Kabum",
+                            "valor_frete": f"{valor_frete:.2f}",
+                            "prazo_dias": 4            
+                            })
+        return jsonify(retorno)
             
     except Exception as e:
         return jsonify({"erro": f"{e}"})
