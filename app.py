278a3d5a398656b0efe0fa390b02dd5435faf31a
Felipe Correia Costa <felipe.correia@gkt.grupokabum.com.br>
2025-11-06 09:51:35 -0300

    Fix tipo_frete dimensões

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ def calcular_frete():
     data = request.get_json()
     
     if not data or not "dimensao" in data or not "peso" in data or not "tipo_frete" in data:
-        
         return jsonify({"error": "campos não completos ou erro na sintaxe"}), 400
     
     try:
@@ -17,32 +16,27 @@ def calcular_frete():
         largura = data['dimensao']['largura']
         peso = data['peso']
         
-        
         if tipo_frete == "ninja":
-            if (altura >= 5 and altura <=140) and (largura >= 13 and largura <= 125) and peso >= 0:
+            if (altura >= 10 and altura <=200) and (largura >= 6 and largura <= 140) and peso > 0:
                 
                 valor_frete = (peso * 0.3) /10
                 return jsonify({                       
-                                    "nome":"Entrega Ninja",
-                                    "valor_frete": f"{valor_frete:.2f}",
-                                    "prazo_dias": 6            
+                                "nome":"Entrega Ninja",
+                                "valor_frete": f"{valor_frete:.2f}",
+                                "prazo_dias": 6            
                                 })
             
             else:
                 return jsonify([]),400
                 
-                
         elif tipo_frete == "kabum":   
-            if (altura >= 10 and altura <=200) and (largura >= 6 and largura <= 140) and peso >= 0:
-                
+            if (altura >= 5 and altura <=140) and (largura >= 13 and largura <= 125) and peso > 0:
                 valor_frete = (peso * 0.2) /10
-                return jsonify({
-                                                        
-                                    "nome":"Entrega Kabum",
-                                    "valor_frete": f"{valor_frete:.2f}",
-                                    "prazo_dias": 4            
+                return jsonify({                        
+                                "nome":"Entrega Kabum",
+                                "valor_frete": f"{valor_frete:.2f}",
+                                "prazo_dias": 4            
                                 })
-            
             else:
                 return jsonify([]),400
             
